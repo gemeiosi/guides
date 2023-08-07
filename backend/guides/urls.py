c4fc55d9f21ff95e21705e7771e97f7ec0bb9523
gemeiosi <43901846+gemeiosi@users.noreply.github.com>
2023-08-07 23:34:04 +0300

    crud added

--- backend/guides/urls.py
+++ backend/guides/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import GuideView
 
 urlpatterns = [
-    path('guides/', GuideView.as_view())
+    path('guides/', GuideView.as_view()),
+    path('guides/<int:pk>/', GuideView.as_view())
 ]
--- backend/guides/views.py
+++ backend/guides/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http.response import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -19,3 +18,33 @@ class GuideView(APIView):
             serializer.save()
             return JsonResponse("Guide Added Successfully",          safe=False)
         return JsonResponse("Failed to Add Guide", safe=False)
+
+    def get_guide(self, pk):
+        try:
+            guide = Guide.objects.get(guideId=pk)
+            return guide
+        except Guide.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk=None):
+        if pk:
+            data = self.get_guide(pk)
+            serializer = GuideSerializer(data)
+        else:
+            data = Guide.objects.all()
+            serializer = GuideSerializer(data, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, pk=None):
+        guide_to_update = Guide.objects.get(guideId=pk)
+        serializer = GuideSerializer(instance=guide_to_update, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse("Guide updated Successfully", safe=False)
+        return JsonResponse("Failed To Update Guide")
+
+    def delete(self, request, pk):
+        guide_to_delete = Guide.objects.get(guideId=pk)
+        guide_to_delete.delete()
+        return JsonResponse("Guide Deleted Successfully", safe=False)
