909c54f59efa1facf4903e3cd518f4995c787a92
gemeiosi <43901846+gemeiosi@users.noreply.github.com>
2023-08-07 18:42:42 +0300

    guide model added

--- backend/guides/models.py
+++ backend/guides/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Guides(models.Model):
+    guideId = models.AutoField(primary_key=True)
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    RegistrationNo = models.CharField(max_length=100)
+    Email = models.CharField(max_length=100)
