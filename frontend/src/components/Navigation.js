6143313896d157df4a208a96a98022d08b5e6cd4
gemeiosi <43901846+gemeiosi@users.noreply.github.com>
2023-08-07 23:19:01 +0300

    home, app, navigation pages added

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Navbar,Nav} from 'react-bootstrap';
+import logo from '../static/logo.jpeg'
+import "../App.css";
+import {NavLink} from 'react-router-dom';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+
+const Navigation = () => {
+  return (
+    <div>
+    <Navbar bg="dark" variant="dark" expand="lg" id="my-nav">
+        <Navbar.Brand className="app-logo" href="/">
+            <img
+              src={logo}
+              width="40"
+              height="50"
+              className="d-inline-block align-center"
+              alt="React Bootstrap logo"
+            />{' '}
+            Guide Management System
+        </Navbar.Brand>
+    </Navbar>
+    <div className='sidebar'>
+<CDBSidebar textColor="#333" backgroundColor="#f0f0f0">
+    <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
+      Navigation
+    </CDBSidebarHeader>
+    <CDBSidebarContent>
+      <CDBSidebarMenu>
+        <NavLink exact to="/" activeClassName="activeClicked">
+          <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
+        </NavLink>
+        <NavLink exact to="/guides" activeClassName="activeClicked">
+          <CDBSidebarMenuItem icon="list">Guides List</CDBSidebarMenuItem>
+        </NavLink>
+        <NavLink exact to="/manage" activeClassName="activeClicked">
+          <CDBSidebarMenuItem icon="user">Manage Guides</CDBSidebarMenuItem>
+        </NavLink>
+      </CDBSidebarMenu>
+    </CDBSidebarContent>
+  </CDBSidebar>
+</div>
+    </div>
+  );
+};
+
+export default Navigation;
